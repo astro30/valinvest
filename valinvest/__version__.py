b9b0fd9a032abf4795928b58bc33e89dd363cc94
astro30 <22853551+astro30@users.noreply.github.com>
2020-02-08 15:00:42 +0100

    __version__.py creation

---
+++ valinvest/__version__.py
@@ -0,0 +1,16 @@
+"""
+ __   __   ______     __         __     __   __     __   __   ______     ______     ______  
+/\ \ / /  /\  __ \   /\ \       /\ \   /\ "-.\ \   /\ \ / /  /\  ___\   /\  ___\   /\__  _\ 
+\ \ \'/   \ \  __ \  \ \ \____  \ \ \  \ \ \-.  \  \ \ \'/   \ \  __\   \ \___  \  \/_/\ \/ 
+ \ \__|    \ \_\ \_\  \ \_____\  \ \_\  \ \_\\"\_\  \ \__|    \ \_____\  \/\_____\    \ \_\ 
+  \/_/      \/_/\/_/   \/_____/   \/_/   \/_/ \/_/   \/_/      \/_____/   \/_____/     \/_/ 
+                                                                                            
+"""
+__title__ = 'valinvest'
+__description__ = 'A value investing tool based on Warren Buffett, Joseph Piotroski and Benjamin Graham thoughts'
+__url__ = 'under construction'
+__version__ = '0.0.1'
+__author__ = 'Guillaume Rey'
+__author_email__ = 'guillaume.rey@gadz.org'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Guillaume Rey'
