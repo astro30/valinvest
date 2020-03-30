93b5c477e0f55e8601f7939c6e64b355b623681b
astro30 <22853551+astro30@users.noreply.github.com>
2020-03-30 10:39:49 +0200

    Added a main

--- valinvest/__init__.py
+++ valinvest/__init__.py
@@ -1,4 +1,9 @@
 import logging.config
-from .config import logging_config
+from .config import *
+from .fundamentals import *
+from .main import *
+from .sentiments import *
+from .storage import *
+from .technicals import *
 
 logging.config.dictConfig(logging_config)
