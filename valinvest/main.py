806dd237c124e6621c26f288cb509867a9df1c78
astro30 <22853551+astro30@users.noreply.github.com>
2020-04-01 12:33:57 +0200

    Update main wrapper (could be out of scope soon...)

--- valinvest/main.py
+++ valinvest/main.py
@@ -1,14 +1,14 @@
 from .fundamentals import *
 from .config import SP_500_TICKERS, NASDAQ_100_TICKERS
 
-def get_tickers_scores(ticker_list=SP_500_TICKERS):
+
+def get_tickers_scores(ticker_list=NASDAQ_100_TICKERS):
     res = []
     for ticker in ticker_list:
         try:
-            score = get_scores(ticker).iloc[:,-5:].mean(axis=1).sum()
+            score = Fundamental(ticker).fscore()
             res.append([ticker, score])
             print(ticker, score)
         except Exception as e:
             print(ticker, e)
     return res
-
