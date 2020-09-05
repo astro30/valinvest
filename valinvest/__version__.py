b815471ca5b82274a96171a6e2ce0efe11311461
astro30 <22853551+astro30@users.noreply.github.com>
2020-09-05 10:30:58 +0200

    🔖 Increase version

--- valinvest/__version__.py
+++ valinvest/__version__.py
@@ -9,7 +9,7 @@
 __title__ = 'valinvest'
 __description__ = 'A value investing tool based on Warren Buffett, Joseph Piotroski and Benjamin Graham thoughts'
 __url__ = 'https://github.com/astro30/valinvest'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Guillaume Rey'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 Guillaume Rey'
